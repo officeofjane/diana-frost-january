95d5a5fb47be09d6625e5c70b8ae7938d0233982
officeofjane <officeofjane@gmail.com>
2026-01-14 16:25:42 +0800

    scrolly scaffolding

---
+++ src/components/ScrollyWrapper.svelte
@@ -0,0 +1,96 @@
+<script>
+  import Scrolly from '$components/helpers/Scrolly.svelte';
+
+  let scrollIndex = $state(0);
+</script>
+
+<div class="container">
+  <div class="sticky">
+    <div class="wheel-wrapper">
+      <p>flywheel here</p>
+      <p>ScrollIndex: {scrollIndex}</p>
+    </div>
+    <!-- turn bg into component? import all the scrolly steps here? -->
+    <div class="bg-wrapper">
+      <div class="bg red" class:active={scrollIndex === 0}>
+        <p>Background: {scrollIndex} I am red if active</p>
+        <p>Does entry animation still work if active class is toggling opacity? (it's not on load)</p>
+        <p>Should work, tie the animation (opacity and transform) to trigger when the active class is applied to element. But how is the CSS scoped if it's a component?</p>
+      </div>
+      <div class="bg blue" class:active={scrollIndex === 1}>
+        <p>Background: {scrollIndex} I am blue if active</p>
+      </div>
+    </div>
+    
+  </div>
+  <div class="steps-wrapper">
+    
+    <Scrolly bind:value={scrollIndex}>
+      {#each ['Hello', 'Scrollytelling', 'World!'] as text, i}
+        <div class="step" class:active={scrollIndex === i}>
+          <p class="copy">{text}</p>
+        </div>
+      {/each}
+    </Scrolly>
+  </div>
+</div>
+
+<style>
+  .container {
+    max-width: 1440px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 30% 1fr;
+  }
+
+  .sticky {
+    height: 100vh;
+    position: sticky;
+    top: 0;
+    left: 0;
+    grid-column: 1 / span 2;
+    display: grid;
+    grid-template-columns: subgrid;
+  }
+
+  .wheel-wrapper {
+    background-color: aliceblue;
+  }
+
+  .bg-wrapper {
+    background-color: papayawhip;
+    position: relative;
+  }
+
+  .bg {
+    opacity: 0;
+    width: 100%;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .bg.active {
+    opacity: 1;
+  }
+
+  .bg.red {
+    background-color: red;
+  }
+
+  .bg.blue {
+    background-color: blue;
+  }
+
+  .steps-wrapper {
+    grid-column: 2;
+    margin-top: -100vh;
+  }
+
+  .step {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+  }
+</style>

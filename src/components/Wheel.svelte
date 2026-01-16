58c6f818a1a68f3d590ad8d7ff731c64cb6b4dc4
officeofjane <officeofjane@gmail.com>
2026-01-16 17:41:21 +0800

    add nodes

--- src/components/Wheel.svelte
+++ src/components/Wheel.svelte
@@ -1,13 +1,22 @@
 <script>
   import { arc, pie } from "d3-shape";
+  import { range } from "d3-array";
+  import { scaleLinear } from "d3-scale";
   import { data } from "$data/data.js";
   import colours from "$data/colours.json";
 
   let { width, height } = $props();
 
-  const ARC_WIDTH = 24;
+  const MARGIN = 120;
+  const ARC_WIDTH = 21;
+  const NUM_NODES = 50;
+  const NODE_RADIUS = 6;
 
-  const outerRadius = $derived(Math.min(width, height) * 0.5 - 60);
+  const nodeScale = scaleLinear()
+    .domain([0, NUM_NODES])
+    .range([0, 360]);
+
+  const outerRadius = $derived(Math.min(width, height) * 0.5 - MARGIN);
   const innerRadius = $derived(outerRadius - ARC_WIDTH);
 
   const wheelArc = $derived.by(() => {
@@ -25,16 +34,42 @@
   const wheelData = wheelPie(data);
 
   console.log(wheelData);
-
 </script>
 
 <svg viewBox="0, 0, {width}, {height}">
+  <defs>
+    <radialGradient id="gradient">
+      <stop offset="10%" stop-color="gold" />
+      <stop offset="95%" stop-color="red" />
+    </radialGradient>
+    <mask id="mask">
+      <rect x="0" y="0" width={width} height={height} fill="url(#gradient)" />
+    </mask>
+  </defs>
+
   <g 
     class="pie"
     transform="translate({width/2}, {height/2})"
   >
     {#each wheelData as slice}
-      <path d={wheelArc(slice)} stroke="white" fill={colours[slice.data.category]}/>
+      <path 
+        d={wheelArc(slice)} 
+        stroke="white" 
+        fill={colours[slice.data.category]}
+      />
+    {/each}
+  </g>
+
+  <g class="nodes"
+    transform="translate({width/2}, {height/2})"
+  >
+    {#each range(NUM_NODES) as node}
+      <circle class="node" 
+      cx="0" 
+      cy={(outerRadius-ARC_WIDTH/2)}
+      r={NODE_RADIUS} 
+      transform="rotate({nodeScale(node) + 360/NUM_NODES/2})" 
+    />
     {/each}
   </g>
 </svg>
@@ -45,7 +80,7 @@
     height: 100%;
   }
   
-  rect {
-    fill: blue;
+  .node {
+    fill: white;
   }
 </style>
--- src/data/data.js
+++ src/data/data.js
@@ -1,16 +1,61 @@
 const data = [
-  { category: "studio", count: 7},
-  { category: "experiences", count: 5},
-  { category: "content-sales", count: 5},
-  { category: "music", count: 2},
-  { category: "games", count: 3},
-  { category: "channel", count: 4},
-  { category: "fan", count: 2},
-  { category: "publishing", count: 3},
-  { category: "entertainment", count: 5},
-  { category: "stores", count: 2},
-  { category: "products", count: 6},
-  { category: "disney-plus", count: 6}
+  { category: "studio", 
+    count: 7,
+    labels: ["Disney Live Action", "Disney Animation", "Pixar", "Marvel Studios", "Lucasfilm", "20th Century", "Searchlight Pictures"]
+  },
+  { category: "experiences", 
+    count: 5,
+    labels: ["Tokyo Disneyland", "Tokyo Disneysea", "Hotels", "Disney Resort Line", "Disney Cruise Line"]
+  },
+  { category: "content-sales", 
+    count: 5,
+    labels: ["DVD/Blu-ray", "SVOD", "TVOD", "EHV", "Linear"]
+  },
+  { 
+    category: "music", 
+    count: 2,
+    labels: ["Album Releases", "Playlists"]
+  },
+  { 
+    category: "games", 
+    count: 3,
+    labels: ["Mobile Devices", "Console Games", "Computer Games"]
+  },
+  { 
+    category: "channel", 
+    count: 4,
+    labels: ["Disney Channel", "Disney Junior", "D Life", "National Geographic"]
+  },
+  { 
+    category: "fan", 
+    count: 2,
+    labels: ["Fan Fest", "Conventions"]
+  },
+  { 
+    category: "publishing", 
+    count: 3,
+    labels: ["Digital Asset Licensing", "Learning Books", "Books and Magazines"]
+  },
+  { 
+    category: "entertainment", 
+    count: 5,
+    labels: ["Disney Music & Fireworks", "Disney On Ice", "Shiki Musicals", "Concerts", "Immersive Exhibitions"]
+  },
+  { 
+    category: "stores", 
+    count: 2,
+    labels: ["Shop Disney (Online)", "Disney Store Japan"]
+  },
+  { 
+    category: "products", 
+    count: 6,
+    labels: ["Beauty", "Food/Pet", "Fashion", "Home", "Stationery", "Toys"]
+  },
+  { 
+    category: "disney-plus", 
+    count: 6,
+    labels: ["Disney", "Pixar", "Marvel", "Star Wars", "National Geographic", "Star (Original Content)"]
+  }
 ];
 
 export { data };

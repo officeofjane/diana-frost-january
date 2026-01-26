ab0e9e04e34b06163e7a04546f7b950e01b4a863
officeofjane <officeofjane@gmail.com>
2026-01-26 11:42:48 +0800

    set up layout step components

--- src/components/ScrollyWrapper.svelte
+++ src/components/ScrollyWrapper.svelte
@@ -5,7 +5,20 @@
   import Scrolly from '$components/helpers/Scrolly.svelte';
   import Wheel from '$components/Wheel.svelte';
   import Background from '$components/Background.svelte';
-  import Step from '$components/Step.svelte';
+  
+  import Step0 from '$components/layout/Step0.svelte';
+  import Step1 from '$components/layout/Step1.svelte';
+  import Step2 from '$components/layout/Step2.svelte';
+  import Step3 from '$components/layout/Step3.svelte';
+  import Step4 from '$components/layout/Step4.svelte';
+  import Step5 from '$components/layout/Step5.svelte';
+  import Step6 from '$components/layout/Step6.svelte';
+  import Step7 from '$components/layout/Step7.svelte';
+  import Step8 from '$components/layout/Step8.svelte';
+  import Step9 from '$components/layout/Step9.svelte';
+  import Step10 from '$components/layout/Step10.svelte';
+  import Step11 from '$components/layout/Step11.svelte';
+  import Step12 from '$components/layout/Step12.svelte';
 
   const copy = getContext("copy");
 
@@ -14,6 +27,8 @@
   let wheelWrapperWidth = $state(0);
   let wheelWrapperHeight = $state(0);
 
+  const layoutSteps = [Step0, Step1, Step2, Step3, Step4, Step5, Step6, Step7, Step8, Step9, Step10, Step11, Step12];
+
   function slice(index) {
     if (index) {
       const obj = narrative.find(d => d.step === index);
@@ -21,6 +36,8 @@
     } 
     return narrative[0];
   }
+
+  
 </script>
 
 <div class="container">
@@ -39,15 +56,13 @@
       <p>ScrollIndex: {scrollIndex}</p>
       <p>Highlight: {slice(scrollIndex).highlight}</p>
     </div>
-    <!-- single noise gradient bg component with colour props? -->
-    <!-- turn each layout step into a separate component? import all the scrolly steps here? -->
-    <!-- pass down isActive state prop into step layout component? then apply class inside component to trigger animation -->
+    <!-- pass down isActive state prop into step layout component, then apply class inside component to trigger animation -->
     <div class="right">
       {#each narrative as step, i}
+      {@const LayoutStep = layoutSteps[i]}
         <div class="bg" class:active={scrollIndex === i}>
           <Background bgColours={slice(scrollIndex).gradientColours}/>
-          <!-- need to create separate components for each step? -->
-          <Step isActive={scrollIndex === i}/>
+          <LayoutStep isActive={scrollIndex === i}></LayoutStep>
         </div>
       {/each}
     </div>
---
+++ src/components/layout/Step1.svelte
@@ -0,0 +1,64 @@
+<script>
+  let { isActive = false } = $props();
+</script>
+
+<div class={["wrapper", {active: isActive}]}>
+  <div class="image a">
+    i am step 1
+  </div>
+  <div class="image b">
+  </div>
+  <div class="image c">
+  </div>
+  <div class="image d">
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+
+    padding: 10px 0 10px 0;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: 2fr 1fr 1fr;
+    grid-gap: 10px;
+
+  }
+
+  .image {
+    background-color: red;
+  }
+
+  .image.a {
+    grid-column: 2 / span 4;
+    grid-row: 1;
+  }
+
+  .active .image.a {
+    background-color: blue;
+    animation-duration: 0.5s;
+    animation-name: animate-pop;
+    animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
+  }
+
+  .image.b {
+    grid-column: 4 / span 2;
+    grid-row: 2 / span 2;
+  }
+
+  .image.c {
+    grid-column: 8 / span 2;
+    grid-row: 2 / span 1;
+  }
+
+  .image.d {
+    grid-column: 7 / span 2;
+    grid-row: 3 / span 1;
+  }
+</style>

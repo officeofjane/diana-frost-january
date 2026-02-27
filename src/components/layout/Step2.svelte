123945fb06442bcef069ac2cbdf3119abb7a0606
officeofjane <officeofjane@gmail.com>
2026-02-27 13:58:51 +0800

    step2 assets

--- src/components/layout/Step2.svelte
+++ src/components/layout/Step2.svelte
@@ -6,49 +6,52 @@
 
 <div class={["layout-wrapper", {active: isActive}]}>
   <div class="image a">
+    <img src="./images/scrolly/step2_experiences/a_guests-frozen-kingdom.jpg" alt="" />
   </div>
   <div class="image b">
+    <img src="./images/scrolly/step2_experiences/b_olaf.jpg" alt="" />
   </div>
   <div class="image c">
+    <img src="./images/scrolly/step2_experiences/c_parade.jpg" alt="" />
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step2_experiences/d_frozen-fantasy.jpg" alt="" />
+  </div>
+  <div class="image e">
+    <img src="./images/scrolly/step2_experiences/e_frozen-kingdom.jpg" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: red;
+    padding: 100px 0 50px 0;
+    grid-template-rows: 1fr 1fr;
   }
 
   .image.a {
-    grid-column: 2 / span 4;
-    grid-row: 1;
-  }
-
-  .active .image.a {
-    background-color: blue;
-    animation-duration: 0.5s;
-    animation-name: animate-pop;
-    animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
+    grid-column: 2 / span 3;
+    align-self: center;
   }
 
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 6 / span 4;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 2 / span 3;
+    grid-row: 2;
+    align-self: center;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 5 / span 3;
+    grid-row: 2;
+    align-self: end;
+  }
+
+  .image.e {
+    grid-column: 7 / span 3;
+    grid-row: 2;
+    align-self: center;
   }
 </style>

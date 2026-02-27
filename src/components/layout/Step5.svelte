7ec985bea22a2b28a7178ab72820faa158ee2a08
officeofjane <officeofjane@gmail.com>
2026-02-27 14:14:24 +0800

    step5 assets

--- src/components/layout/Step5.svelte
+++ src/components/layout/Step5.svelte
@@ -5,50 +5,71 @@
 </script>
 
 <div class={["layout-wrapper", {active: isActive}]}>
-  <div class="image a">
-  </div>
   <div class="image b">
+    <img src="./images/scrolly/step5_games/b_nintendo-gameplay.png" alt="" />
+  </div>
+  <div class="image a">
+    <img src="./images/scrolly/step5_games/a_nintendo-3ds.jpg" alt="" />
   </div>
   <div class="image c">
+    <img src="./images/scrolly/step5_games/c_nintendo-gameplay-2.jpg" alt="" />
   </div>
-  <div class="image d">
+  <div class="image d transparent">
+    <img src="./images/scrolly/step5_games/d_elsa-tsum.png" alt="" />
+  </div>
+  <div class="image e">
+    <img src="./images/scrolly/step5_games/e_kingdom-hearts-iii-1.jpg" alt="" />
+  </div>
+  <div class="image f">
+    <img src="./images/scrolly/step5_games/f_kingdom-hearts-iii-2.jpg" alt="" />
+  </div>
+  <div class="image g transparent">
+    <img src="./images/scrolly/step5_games/g_anna-tsum.png" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
+    padding: 100px 0 30px 0;
     grid-template-rows: 2fr 1fr 1fr;
   }
 
-  .image {
-    background-color: red;
-  }
-
   .image.a {
-    grid-column: 2 / span 4;
+    grid-column: 2 / span 2;
     grid-row: 1;
   }
 
-  .active .image.a {
-    background-color: blue;
-    animation-duration: 0.5s;
-    animation-name: animate-pop;
-    animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
-  }
-
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 3 / span 3;
+    grid-row: 1;
+    align-self: center;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 7 / span 3;
+    grid-row: 1;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 2 / span 2;
+    grid-row: 3;
+    align-self: end;
+  }
+
+  .image.e {
+    grid-column: 4 / span 3;
+    grid-row: 2 / span 2;
+    align-self: center;
+  }
+
+  .image.f {
+    grid-column: 7 / span 3;
+    grid-row: 3;
+    align-self: end;
+  }
+
+  .image.g {
+    grid-column: 9 / span 2;
+    grid-row: 2;
   }
 </style>

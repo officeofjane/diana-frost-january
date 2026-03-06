22a9017ea5009b3e0552c8f8e20f9b17a31d1890
officeofjane <officeofjane@gmail.com>
2026-03-06 17:39:34 +0800

    test video

--- src/components/layout/Step0.svelte
+++ src/components/layout/Step0.svelte
@@ -5,55 +5,25 @@
 </script>
 
 <div class={["layout-wrapper", {active: isActive}]}>
-  <div class="image a sticker">
-    <img src="./images/elsa-costume.png" alt="" />
-  </div>
-  <div class="image b">
-  </div>
-  <div class="image c">
-  </div>
-  <div class="image d sticker">
-    <img src="./images/elsa-bottle-sticker.png" alt="" />
+  <div class="video-wrapper">
+    <video src="./videos/00_frozen-trailer-trim.mp4" autoplay  loop muted>
+      <track kind="captions" />
+    </video> 
   </div>
 </div>
 
 <style>
   .layout-wrapper {
     padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
   }
 
-  .image.a {
-    grid-column: 2 / span 4;
-    grid-row: 1;
+  .video-wrapper {
+    grid-column: 2 / 10;
+    align-self: center;
+    background-color: whitesmoke;
   }
 
-  .active .image.a {
-    animation-duration: 0.5s;
-    animation-name: animate-pop;
-    animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
-  }
-
-  .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
-  }
-
-  .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
-  }
-
-  .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
-  }
-
-  .image.sticker img {
-    box-shadow: none;
-  }
-
-  .image.sticker {
-    filter: drop-shadow(4px 8px 6px hsl(216deg 60% 28% / 0.5));
+  video {
+    width: 100%;
   }
 </style>

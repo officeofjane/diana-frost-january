ccebea2e9113b874721573f4bac2bb8efa1c4804
officeofjane <officeofjane@gmail.com>
2026-03-06 18:37:33 +0800

    step1 assets

--- src/components/layout/Step1.svelte
+++ src/components/layout/Step1.svelte
@@ -6,49 +6,52 @@
 
 <div class={["layout-wrapper", {active: isActive}]}>
   <div class="image a">
+    <video src="./videos/01_olafs-frozen-adventure.mp4" autoplay loop muted>
+      <track kind="captions" />
+    </video> 
   </div>
   <div class="image b">
+    <img src="./images/scrolly/step1_studio/b_olaf-frozen-adventure.png" alt=""/>
   </div>
   <div class="image c">
+    <img src="./images/scrolly/step1_studio/c_frozen-fever-poster.png" alt=""/>
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step1_studio/d_olafs-frozen-adventure-poster.png" alt=""/>
   </div>
 </div>
 
 <style>
   .layout-wrapper {
     padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: whitesmoke;
+    grid-template-rows: 3fr 1fr 1fr 1fr;
   }
 
   .image.a {
     grid-column: 2 / span 4;
     grid-row: 1;
+    align-self: center;
   }
 
   .active .image.a {
-    background-color: blue;
     animation-duration: 0.5s;
     animation-name: animate-pop;
     animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
   }
 
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 6 / span 2;
+    grid-row: 1;
+    align-self: end;
   }
 
   .image.c {
     grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-row: 2 / span 2;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 4 / span 4;
+    grid-row: 3 / span 2;
   }
 </style>

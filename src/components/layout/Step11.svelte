f259a4d750ad253349ceb5317fa3c921543afcfa
officeofjane <officeofjane@gmail.com>
2026-03-07 16:38:45 +0800

    step11 assets

--- src/components/layout/Step11.svelte
+++ src/components/layout/Step11.svelte
@@ -6,32 +6,34 @@
 
 <div class={["layout-wrapper", {active: isActive}]}>
   <div class="image a">
+    <img src="./images/scrolly/step11_products/toy-elsa.png" alt="" /> 
   </div>
   <div class="image b">
+    <img src="./images/scrolly/step11_products/toy-olaf.png" alt="" /> 
   </div>
   <div class="image c">
+    <img src="./images/scrolly/step11_products/b_frozen2cafe.jpg" alt="" />
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step11_products/elsa-bottle-sticker.png" alt="" />
+  </div>
+  <div class="image e">
+    <img src="./images/scrolly/step11_products/c_frozen-oh-my-cafe-2.jpg" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: red;
+    padding: 100px 0 100px 0;
+    grid-template-rows: 1fr 1fr;
   }
 
   .image.a {
-    grid-column: 2 / span 4;
+    grid-column: 2 / span 3;
     grid-row: 1;
   }
 
   .active .image.a {
-    background-color: blue;
     animation-duration: 0.5s;
     animation-name: animate-pop;
     animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
@@ -39,16 +41,25 @@
 
   .image.b {
     grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-row: 1;
+    align-self: center;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 6 / span 3;
+    grid-row: 1;
+    align-self: end;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 3 / span 2;
+    grid-row: 2;
+    align-self: start;
+  }
+
+  .image.e {
+    grid-column: 5 / span 5;
+    grid-row: 2;
+    align-self: center;
   }
 </style>

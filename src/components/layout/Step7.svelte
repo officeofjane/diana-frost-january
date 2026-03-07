b52ad3c022b2a8de90a74b429255c6b6bd0c7fdf
officeofjane <officeofjane@gmail.com>
2026-03-07 12:34:22 +0800

    step7 assets

--- src/components/layout/Step7.svelte
+++ src/components/layout/Step7.svelte
@@ -6,49 +6,59 @@
 
 <div class={["layout-wrapper", {active: isActive}]}>
   <div class="image a">
+    <img src="./images/scrolly/step7_fan/frozen_fanfest.jpeg" alt="" />
   </div>
-  <div class="image b">
-  </div>
+  <!-- <div class="image b">
+    <img src="./images/scrolly/step7_fan/shopping-1.png" alt="" />
+  </div> -->
   <div class="image c">
+    <img src="./images/scrolly/step7_fan/frozen_cosplay-1.png" alt="" />
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step7_fan/frozen_cosplay-2.png" alt="" />
+  </div>
+  <div class="image e">
+    <img src="./images/scrolly/step7_fan/elsa-costume.png" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: red;
+    padding: 50px 0 50px 0;
+    grid-template-rows: 2fr 3fr;
   }
 
   .image.a {
-    grid-column: 2 / span 4;
+    grid-column: 2 / span 5;
     grid-row: 1;
   }
 
   .active .image.a {
-    background-color: blue;
     animation-duration: 0.5s;
     animation-name: animate-pop;
     animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
   }
 
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 7 / span 4;
+    grid-row: 1 / span 2;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 2 / span 3;
+    grid-row: 2;
+    align-self: center;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 4 / span 3;
+    grid-row: 2;
+    align-self: end;
+  }
+
+  .image.e {
+    grid-column: 7 / span 5;
+    grid-row: 2;
+    align-self: center;
   }
 </style>

eb9cd8095e75c4f02c06f3f951b16a0be3fd14aa
officeofjane <officeofjane@gmail.com>
2026-03-07 16:50:11 +0800

    step12 assets

--- src/components/layout/Step12.svelte
+++ src/components/layout/Step12.svelte
@@ -6,23 +6,25 @@
 
 <div class={["layout-wrapper", {active: isActive}]}>
   <div class="image a">
+    <video src="./videos/12_once-upon-a-snowman.mp4" autoplay loop muted>
+      <track kind="captions" />
+    </video>
   </div>
   <div class="image b">
+    <img src="./images/scrolly/step12_disney-plus/b_scene.webp" alt="" />
   </div>
   <div class="image c">
+    <img src="./images/scrolly/step12_disney-plus/c_once-upon-a-snowman.webp" alt="" />
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step12_disney-plus/d_onceuponasnowman_disneyplus_poster.jpg" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: red;
+    padding: 100px 0 100px 0;
+    grid-template-rows: 1fr 1fr;
   }
 
   .image.a {
@@ -31,24 +33,26 @@
   }
 
   .active .image.a {
-    background-color: blue;
     animation-duration: 0.5s;
     animation-name: animate-pop;
     animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
   }
 
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 6 / span 3;
+    grid-row: 1;
+    align-self: center;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 4 / span 4;
+    grid-row: 2;
+    align-self: end;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 8 / span 2;
+    grid-row: 2;
+    align-self: center;
   }
 </style>

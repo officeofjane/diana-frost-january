6f9013c8d91fcc35576e580f60a58c6f2d320066
officeofjane <officeofjane@gmail.com>
2026-03-07 11:32:43 +0800

    step4 assets

--- src/components/layout/Step4.svelte
+++ src/components/layout/Step4.svelte
@@ -6,49 +6,53 @@
 
 <div class={["layout-wrapper", {active: isActive}]}>
   <div class="image a">
+    <img src="./images/scrolly/step4_music/a_soundtrack-deluxe.jpg" alt="" />
   </div>
   <div class="image b">
+    <img src="./images/scrolly/step4_music/b_soundtrack.jpg" alt="" />
   </div>
   <div class="image c">
+    <video src="./videos/04_let-it-go.mp4" autoplay loop muted>
+      <track kind="captions" />
+    </video> 
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step4_music/d_soundtrack-back.jpg" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: red;
+    padding: 80px 0 80px 0;
+    grid-template-rows: 1fr 1fr;
   }
 
   .image.a {
-    grid-column: 2 / span 4;
+    grid-column: 3 / span 3;
     grid-row: 1;
+    align-self: end;
   }
 
   .active .image.a {
-    background-color: blue;
     animation-duration: 0.5s;
     animation-name: animate-pop;
     animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
   }
 
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 7 / span 3;
+    grid-row: 1;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 2 / span 5;
+    grid-row: 2;
+    align-self: center;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 8 / span 2;
+    grid-row: 2;
+    /* align-self: center;s */
   }
 </style>

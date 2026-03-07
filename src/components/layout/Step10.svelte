86b808a93fb92cd29b119fa80cccf746b0923fba
officeofjane <officeofjane@gmail.com>
2026-03-07 16:38:19 +0800

    step10 assets

--- src/components/layout/Step10.svelte
+++ src/components/layout/Step10.svelte
@@ -5,24 +5,24 @@
 </script>
 
 <div class={["layout-wrapper", {active: isActive}]}>
-  <div class="image a">
+  <div class="image a circle">
+    <img src="./images/scrolly/step10_stores/b_frozen-wedding-dress-collection.jpg" alt="" />
   </div>
-  <div class="image b">
+  <div class="image b circle">
+    <img src="./images/scrolly/step10_stores/a_alfred-angelo-for-disney.jpg" alt="" />    
   </div>
   <div class="image c">
+    <img src="./images/scrolly/step10_stores/c_alfred-angelo-disneys-elsa-frozen-wedding-dress.jpg" alt="" />
   </div>
   <div class="image d">
+    <img src="./images/scrolly/step10_stores/d_frozen-wedding-anna-2.jpg" alt="" />
   </div>
 </div>
 
 <style>
   .layout-wrapper {
-    padding: 10px 0 10px 0;
-    grid-template-rows: 2fr 1fr 1fr;
-  }
-
-  .image {
-    background-color: red;
+    padding: 50px 0 50px 0;
+    grid-template-rows: 1fr 1fr;
   }
 
   .image.a {
@@ -31,24 +31,26 @@
   }
 
   .active .image.a {
-    background-color: blue;
     animation-duration: 0.5s;
     animation-name: animate-pop;
     animation-timing-function: cubic-bezier(.26, .53, .74, 1.48);
   }
 
   .image.b {
-    grid-column: 4 / span 2;
-    grid-row: 2 / span 2;
+    grid-column: 5 / span 3;
+    grid-row: 1;
+    align-self: end;
   }
 
   .image.c {
-    grid-column: 8 / span 2;
-    grid-row: 2 / span 1;
+    grid-column: 6 / span 4;
+    grid-row: 2;
+    align-self: center;
   }
 
   .image.d {
-    grid-column: 7 / span 2;
-    grid-row: 3 / span 1;
+    grid-column: 4 / span 2;
+    grid-row: 2;
+    align-self: end;
   }
 </style>

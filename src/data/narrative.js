d2b00a81a775a0e4338a8629ce23b648802d7eec
officeofjane <officeofjane@gmail.com>
2026-03-09 14:01:08 +0800

    clean up

--- src/data/narrative.js
+++ src/data/narrative.js
@@ -4,68 +4,55 @@ import { symbolPlus } from "d3-shape";
 const narrative = [
   { step: 0, 
     highlight: "all", 
-    rotation: 0,
-    gradientColours: [colours["disney-plus"], colours["studio"]] 
+    rotation: 0
   },
   { step: 1, 
     highlight: "studio", 
-    rotation: 8.5,
-    gradientColours: [colours["studio"], colours["experiences"]] 
+    rotation: 8.5
   },
   { step: 2, 
     highlight: "experiences", 
-    rotation: 2.5,
-    gradientColours: [colours["experiences"], colours["content-sales"]] 
+    rotation: 2.5
   },
   { step: 3, 
     highlight: "content-sales", 
-    rotation: -2.5,
-    gradientColours: [colours["content-sales"], colours["music"]] 
+    rotation: -2.5
   },
   { step: 4, 
     highlight: "music", 
-    rotation: -6,
-    gradientColours: [colours["music"], colours["games"]] 
+    rotation: -6
   },
   { step: 5, 
     highlight: "games", 
-    rotation: -8.5,
-    gradientColours: [colours["games"], colours["channel"]] 
+    rotation: -8.5
   },
   { step: 6, 
     highlight: "channel", 
-    rotation: -12,
-    gradientColours: [colours["channel"], colours["fan"]] 
+    rotation: -12
   },
   { step: 7, 
     highlight: "fan", 
-    rotation: -15,
-    gradientColours: [colours["fan"], colours["publishing"]] 
+    rotation: -15
   },
   { step: 8, 
     highlight: "publishing", 
-    rotation: -17.5,
-    gradientColours: [colours["publishing"], colours["entertainment"]] 
+    rotation: -17.5
   },
   { step: 9, 
     highlight: "entertainment", 
-    rotation: -21.5,
-    gradientColours: [colours["entertainment"], colours["stores"]] 
+    rotation: -21.5
   },
   { step: 10, 
     highlight: "stores", 
-    rotation: -25,
-    gradientColours: [colours["stores"], colours["products"]] 
+    rotation: -25
   },
   { step: 11, 
     highlight: "products", 
-    rotation: -29,
-    gradientColours: [colours["products"], colours["disney-plus"]] 
+    rotation: -29
   },
   { step: 12, 
     highlight: "disney-plus", 
-    rotation: -35,
-    gradientColours: [colours["disney-plus"], colours["studio"]] 
+    rotation: -35
   }
 ];
 
